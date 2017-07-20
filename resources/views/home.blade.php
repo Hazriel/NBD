69d8b444d0d4821ec11b8b1a2739e715754b1dd1
Gabriel 'Hazriel' Bertini <gabriel.bertini@epita.fr>
2017-07-20 14:00:43 +0200

    [TEAMSPEAK] Added ts view on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,9 +41,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-3">
-            <div class="nbd-section server-info">
-                <p>Server information</p>
+        <div class="col-lg-3 server-info">
+            <div class="nbd-section">
+                <div id="ts3viewer_1101575" style=""> </div>
+                <script src="https://static.tsviewer.com/short_expire/js/ts3viewer_loader.js"></script>
+                <script>
+                    var ts3v_url_1 = "https://www.tsviewer.com/ts3viewer.php?ID=1101575&text=e3e3e3&text_size=12&text_family=1&text_s_color=ffffff&text_s_weight=normal&text_s_style=normal&text_s_variant=normal&text_s_decoration=none&text_i_color=&text_i_weight=normal&text_i_style=normal&text_i_variant=normal&text_i_decoration=none&text_c_color=&text_c_weight=normal&text_c_style=normal&text_c_variant=normal&text_c_decoration=none&text_u_color=ffffff&text_u_weight=normal&text_u_style=normal&text_u_variant=normal&text_u_decoration=none&text_s_color_h=&text_s_weight_h=bold&text_s_style_h=normal&text_s_variant_h=normal&text_s_decoration_h=none&text_i_color_h=ffffff&text_i_weight_h=bold&text_i_style_h=normal&text_i_variant_h=normal&text_i_decoration_h=none&text_c_color_h=&text_c_weight_h=normal&text_c_style_h=normal&text_c_variant_h=normal&text_c_decoration_h=none&text_u_color_h=&text_u_weight_h=bold&text_u_style_h=normal&text_u_variant_h=normal&text_u_decoration_h=none&hideEmptyChannels&iconset=default_mono_2014";
+                    ts3v_display.init(ts3v_url_1, 1101575, 100);
+                </script>
             </div>
         </div>
     </div>
