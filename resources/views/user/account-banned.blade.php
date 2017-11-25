d9fcda677a890e3059eef25337d69305a21061ba
Gabriel 'Hazriel' Bertini <gabriel.bertini@epita.fr>
2017-11-25 16:51:12 +0100

    [VIEWS] Added new views for user account state reminder

---
+++ resources/views/user/account-banned.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hazriel
+ * Date: 25-Nov-17
+ * Time: 15:36
+ */
---
+++ resources/views/user/account-not-activated.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="nbd-section">
+                <div class="nbd-section-header">
+                    <h1>You must verify your email</h1>
+                </div>
+                <div class="nbd-section-body">
+                    <p>An email has been sent to your mailbox. You must click the link in the mail.</p>
+                    <p>If you didn't receive any email, we'll send you one again if you click <a href="#">here</a>.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
