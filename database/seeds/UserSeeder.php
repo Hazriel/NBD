a70cb4fe85c3dc6f07c45061dc18f946420fdf1c
Gabriel 'Hazriel' Bertini <gabriel.bertini@epita.fr>
2017-12-09 16:53:39 +0100

    [USER] Added user seeder that generates an admin

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $user = \App\User::create([
+            'username' => 'Hazriel',
+            'email' => 'hazrielgaming@gmail.com',
+            'password' => bcrypt('danstaface'),
+            'activated' => true,
+            'banned' => false
+        ]);
 
+        $admin = \App\Role::where('slug', 'admin')->first();
+
+        if ($admin != null) {
+            \Illuminate\Support\Facades\DB::table('role_user')->insert([
+                'user_id' => $user->id,
+                'role_id' => $admin->id
+            ]);
+        }
     }
 }
